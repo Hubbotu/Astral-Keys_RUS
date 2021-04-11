8ea93257b3983d26b377f1e9d9591dc750686374
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-04-11 14:16:38 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,57 @@
+local L = select(2, ...)[2]('ruRU')
+
+-- Default string for keystone
+L['KEYSTONE'] = 'Ключ:'
+
+-- Default tab names
+L['GUILD'] = 'ГИЛЬДИЯ'
+L['FRIENDS'] = 'ДРУЗЬЯ'
+L['PARTY'] = 'ГРУППА'
+L['RAID'] = 'РЕЙД'
+
+-- Column Headers
+L['LEVEL'] = 'УРОВЕНЬ'
+L['DUNGEON'] = 'ПОДЗЕМЕЛЬЕ'
+L['CHARACTER'] = 'ПЕРСОНАЖ'
+
+-- Subsection Headers
+L['CHARACTERS'] = 'ПЕРСОНАЖИ'
+L['AFFIXES'] = 'АФФИКСЫ'
+
+-- Character Labels
+L['CURRENT_KEY'] = 'ТЕКУЩИЙ КЛЮЧ'
+L['WEEKLY_BEST'] = 'ЕЖЕНЕДЕЛЬНЫЙ ЛУЧШИЙ'
+
+L['CHARACTER_DUNGEON_NOT_RAN'] = 'Нет мифик+ забегов'
+L['CHARACTER_KEY_NOT_FOUND'] = 'Ключ не найден'
+
+
+-- Dropdown menu selections
+L['REPORT_TO'] = 'Сообщите'
+L['Whisper'] = 'Шепот'
+L['INVITE'] = 'Пригласить'
+L['SUGGEST_INVITE'] = "Предложить приглашение"
+L['REQUEST_INVITE'] = "Запросить приглашение"
+L['CANCEL'] = 'ОТМЕНИТЬ'
+L['NEW_LIST_DESCRIPTION'] = 'Название нового списка'
+L['CREATE_NEW_LIST'] = 'Создать новый список'
+L['REMOVE_UNIT_FROM_LIST'] = 'Удалить из списка'
+L['ADD_TO_LIST'] = 'Добавить объект(ы) в список'
+L['ADD_REMOVE_LIST'] = 'Добавить или удалить список'
+L['DELETE_LIST'] = 'Удалить список'
+L['LIST_ADD_HELPER_TEXT'] = 'Добавьте в список игрока, щелкнув ПКМ на игроке в вашем списке ГИЛЬДИИ или ДРУЗЬЯХ'
+L['OKAY'] = OKAY
+
+-- Announce messages
+L['ANNOUNCE_NEW_KEY'] = 'Astral Keys: новый ключ %s'
+L['NO_KEY'] = 'Нет ключа'
+L['KEYS_RESPOND_ON_NO_KEY'] = 'Отвечайте, даже если у вас нет ключа'
+
+
+-- Search field texts
+L['FILTER_TEXT_DUNGEON'] = 'Фильтр по подземельям'
+L['FILTER_TEXT_CHARACTER'] = 'Фильтр по имени персонажа'
+
+-- Options
+L['!KEYS_DESC'] = 'Отвечайте на команду !keys в следующих каналах чата'
+L['EXPANDED_TOOLTIP'] = 'Показать описание аффикса во всплывающей подсказке'
